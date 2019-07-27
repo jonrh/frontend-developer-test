0a9dba2b22500e79c96989826bb574dd72255fa6
Jón Rúnar Helgason <dev@jonrh.is>
2019-07-27 13:09:54 +0100

    Moved contents of App.tsx into source/Main.tsx
    The plan is to have all components be located in the source/ folder. From what I can tell Expo expects the entry point of the app to be the component App located in the root of the project. Now App.tsx has basically become a proxy shell for Main.tsx.
    
    As more files get added to the project having a separate source/ folder will make things a bit more organised I think.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,2 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Akur</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+import Main from "./source/Main";
+export default Main;

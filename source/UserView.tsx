66bf97bd5a3ed53536c0e472c83a02ff7bb222b5
Jón Rúnar Helgason <dev@jonrh.is>
2019-07-28 19:01:55 +0100

    Images are now shown one after another on the user view
    UserView component now displays user images one after another if the user has images. For now this is just a very rough implementation with a static 500 unit height of images. I discovered that images can range wildly in sizes and aspect ratios. I played around a bit with the different `resizeMode` props on the <Image> component. I think "cover" is the default (so it doesn't have to be specified as a prop) and it was the only one that looked remotely okay. However it's this is far from perfect. It faces appear on the boundaries of images they could be cropped out. This is something that in practice I would say should be solved on the backend. Better yet, by the user at the time of upload to give a preview of how the image will roughly turn out.
    
    Fixed a minor spelling mistake on a variable, `interetsView` -> `interestView`.

--- source/UserView.tsx
+++ source/UserView.tsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-import { User } from "./Types";
+import { User, UserPhoto } from "./Types";
 
-interface Props {
+interface PhotoProps {
+  photo: UserPhoto;
+}
+
+/** Displays a plain view of a user's photo */
+const Photo: React.FC<PhotoProps> = props => {
+  return <Image style={styles.image} source={{ uri: props.photo.url }} />;
+};
+
+interface UserViewProps {
   user: User;
 }
 
-/** A component that displays a user profile */
-const UsersView: React.FC<Props> = props => {
-  const { user } = props;
-  const { name, age, gender, sexuality, about, desires, interests } = user.info;
+/** Displays a user profile. Name, age, gender, sexuality and an image for example. */
+const UsersView: React.FC<UserViewProps> = props => {
+  const { name, age, gender, sexuality, about, desires, interests } = props.user.info;
+  const { photos } = props.user;
 
   // If desires or interests arrays are defined display it. Otherwise don't show anything.
   const desiresView = desires ? <Text>{`Desires: ${desires.join(", ")}`}</Text> : null;
-  const interetsView = interests ? <Text>{`Interets: ${interests.join(", ")}`}</Text> : null;
+  const interestView = interests ? <Text>{`Interets: ${interests.join(", ")}`}</Text> : null;
+
+  // Display all of the user's images one after another if they exist.
+  const images = photos ? photos.map((photo, i) => <Photo photo={photo} key={i} />) : null;
 
   return (
     <View style={styles.container}>
@@ -22,18 +34,28 @@ const UsersView: React.FC<Props> = props => {
       <Text>{`${age}y ${gender}, ${sexuality}`}</Text>
       <Text>{`About: ${about}`}</Text>
       {desiresView}
-      {interetsView}
+      {interestView}
+      {images}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    marginLeft: 10,
+    marginRight: 10,
     marginBottom: 20,
   },
 
   name: {
     fontSize: 25,
+  },
+
+  image: {
+    height: 500,
+    width: null,
+
+    marginTop: 10,
   },
 });
 

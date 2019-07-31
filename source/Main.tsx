678be0a7625dbf9b8201d16ded65947e856541c6
Jón Rúnar Helgason <dev@jonrh.is>
2019-07-31 18:57:52 +0100

    Added a new file: Main.tsx. Initial version for app navigation
    This file is intended to be the start of the app navigation. There was a file previously called Main.tsx. Instead, I chose to rename that file (to ListOfUsers) so git history would be retained.

---
+++ source/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ListOfUsers from "./components/ListOfUsers";
+
+/** Initial version of app navigation. For now just a simple redirect. */
+const Main: React.FC = () => {
+  return <ListOfUsers />;
+};
+
+export default Main;

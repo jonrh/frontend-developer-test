3290ffeebf79f5df9eaeb7df1af1503dd209ec71
Jón Rúnar Helgason <dev@jonrh.is>
2019-08-02 19:03:46 +0100

    Basic bottom tab bar navigation in place
    In this change I now have a rudimentary tab bar on the bottom. The rough plan is to have 3 tabs/screens:
    
    + Chat: a list of mutual matches (two users approved each other) and chat with them
    + Decide: a screen to reject/skip/approve other users
    + Settings: vague idea at the moment, but idea being to provide some debug tools like resetting the matching, so it's possible to begin again.
    
    Currently the tab bar doesn't have any text labels, just icons in the pink/purple. The icons used are SimpleLineIcons. The Chat screen has a chat bubble icon, the Decide screen has a heart icon, and the Settings screen has an circle with an exclamation mark in the middle.
    
    See:
    https://expo.github.io/vector-icons/

--- source/Constants.ts
+++ source/Constants.ts
@@ -1,2 +1,5 @@
 /** Used when developing. If true bright debug background colours are turned on components. */
 export const isDebug: boolean = false;
+
+/** The main brand theme colour for the app, a bright purple/pink */
+export const THEME_COLOUR_PURPLE = "#f072ff";
--- source/Main.tsx
+++ source/Main.tsx
@@ -1,21 +1,88 @@
 import React from "react";
+import { Text, View } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { SimpleLineIcons } from "@expo/vector-icons";
 
 import DecideUsers from "./components/DecideUsers";
 import ListOfUsers from "./components/ListOfUsers";
 import { SafeAreaView } from "react-native";
 import SwipeTester from "./components/SwipeTester";
 import SwipeTester2 from "./components/SwipeTester2";
+import { THEME_COLOUR_PURPLE } from "./Constants";
 
-/** Initial version of app navigation. For now just a simple redirect. */
-const Main: React.FC = () => {
+function ListOfUsersScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      {/*<ListOfUsers />*/}
-      <DecideUsers />
-      {/*<SwipeTester />*/}
-      {/*<SwipeTester2 />*/}
+      <ListOfUsers />
     </SafeAreaView>
   );
-};
+}
 
-export default Main;
+function DecideUsersScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <DecideUsers />
+    </SafeAreaView>
+  );
+}
+
+function ChatScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>This screen will show matches and other open chats</Text>
+      <SimpleLineIcons name="bubble" size={32} color="black" />
+      <SimpleLineIcons name="heart" size={32} color="black" />
+      <SimpleLineIcons name="exclamation" size={32} color="black" />
+    </SafeAreaView>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>Settings</Text>
+    </SafeAreaView>
+  );
+}
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Chat: ChatScreen,
+    Decide: DecideUsersScreen,
+    Settings: SettingsScreen,
+
+    // Debug screens
+    List: ListOfUsersScreen,
+    Swipe1: SwipeTester,
+    Swipe2: SwipeTester2,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+
+        // Give the appropriate tab bar icon to each screen
+        if (routeName === "Chat") {
+          return <SimpleLineIcons name="bubble" size={32} color={tintColor} />;
+        } else if (routeName === "Decide") {
+          return <SimpleLineIcons name="heart" size={32} color={tintColor} />;
+        } else if (routeName === "Settings") {
+          return <SimpleLineIcons name="exclamation" size={32} color={tintColor} />;
+        } else {
+          // As a placeholder while I develop, show some icon for dev screens
+          return <SimpleLineIcons name="emotsmile" size={32} color={tintColor} />;
+
+          // Before I finish we should return null here, uncomment when I'm done testing
+          // return null;
+        }
+      },
+    }),
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: THEME_COLOUR_PURPLE,
+      inactiveTintColor: "gray",
+    },
+  }
+);
+
+export default createAppContainer(TabNavigator);
